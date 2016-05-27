4d06aa2fe991235728d34d20e7391ffe4c57a44c
rksdna <rksdna@mail.ru>
2016-05-27 19:28:34 +0400

    Volume control added

--- main.c
+++ main.c
@@ -24,7 +24,10 @@
 #include <stm32/syscfg.h>
 #include <stm32/usart.h>
 #include <stm32/gpio.h>
+#include <stm32/exti.h>
+#include <stm32/nvic.h>
 #include <stm32/rcc.h>
+#include <stm32/tim.h>
 #include <threads.h>
 #include <timers.h>
 #include <debug.h>
@@ -40,10 +43,65 @@ void debug_put(void *data, char value)
 }
 
 struct stream debug_stream = {debug_put, 0};
+static u32_t event;
+static u32_t data;
+struct hid_report report;
+
+void irq6_handler(void)
+{
+    u32_t count = TIM14->CNT;
+    TIM14->EGR = TIM_EGR_UG;
+
+    switch (event)
+    {
+    case 0:
+        event = 1;
+        break;
+
+    case 1:
+        if ((count > 1350 - 10) && (count < 1350 + 10))
+        {
+            event = 2;
+            break;
+        }
+        if ((count > 1125 - 10) && (count < 1125 + 10))
+        {
+            event = 34;
+            break;
+        }
+        event = 0;
+        break;
+
+    default:
+        if (event < 34)
+        {
+            if ((count > 225 - 10) && (count < 225 + 10))
+            {
+                data = data << 1 | 1;
+                event++;
+                break;
+            }
+            if ((count > 112 - 10) && (count < 112 + 10))
+            {
+                data = data << 1;
+                event++;
+                break;
+            }
+            event = 0;
+        }
+    }
+
+    EXTI->PR = EXTI_PR_PR3;
+}
+
+
+static u32_t data_ready(void)
+{
+    return event >= 34;
+}
 
 void main(void)
 {
-    struct hid_report report;
 
     RCC->CR = RCC_CR_HSEON | RCC_CR_HSION;
     wait_status(&RCC->CR, RCC_CR_HSERDY);
@@ -55,24 +113,35 @@ void main(void)
     RCC->CFGR = RCC_CFGR_PLLMUL12 | RCC_CFGR_PLLXTPRE | RCC_CFGR_PLLSRC_HSE_PREDIV | RCC_CFGR_SW_PLL;
     RCC->CFGR3 = 0;
 
-    RCC->AHBENR = RCC_AHBENR_SRAMEN | RCC_AHBENR_FLITFEN | RCC_AHBENR_GPIOAEN;
-    RCC->APB1ENR = RCC_APB1ENR_USART2EN | RCC_APB1ENR_USBEN;
+    RCC->AHBENR = RCC_AHBENR_SRAMEN | RCC_AHBENR_FLITFEN | RCC_AHBENR_GPIOAEN | RCC_AHBENR_GPIOBEN;
+    RCC->APB1ENR = RCC_APB1ENR_USART2EN | RCC_APB1ENR_USBEN | RCC_APB1ENR_TIM14EN;
     RCC->APB2ENR =  RCC_APB2ENR_SYSCFGCOMPEN;
 
     SYSCFG->CFGR1 = SYSCFG_CFGR1_PA11_PA12_RMP;
+    SYSCFG->EXTICR[0] = SYSCFG_EXTICR1_EXTI3_PB;
+
+    EXTI->IMR = EXTI_IMR_MR3;
+    EXTI->FTSR = EXTI_FTSR_TR3;
+    NVIC->ISER = NVIC_ISER_SETENA_6;
 
     GPIOA->MODER = GPIO_MODER_MODER0_0 | GPIO_MODER_MODER14_1;
     GPIOA->PUPDR = 0;
-
     GPIOA->AFRH = (1 << 6 * 4);
+
+    GPIOB->PUPDR = GPIO_PUPDR_PUPDR3_0;
 
     USART2->CR1 = USART_CR1_UE | USART_CR1_TE;
     USART2->CR2 = 0;
     USART2->BRR = 48000000 / 115200 + 1;
 
+    TIM14->ARR = 65535;
+    TIM14->PSC = 480 - 1;
+    TIM14->CR1 = TIM_CR1_CEN;
+
     start_timers_clock(48000);
 
     debug("hello\n");
+
 
     start_hid_service();
     while (1)
@@ -81,13 +150,34 @@ void main(void)
         debug("connected\n");
         while (has_hid_connection())
         {
-            report.keys[0] = 0x20;
-            write_hid_report(&report);
-            report.keys[0] = 0x00;
-            write_hid_report(&report);
-            sleep(1000);
-        }
+            wait_signal(data_ready);
+            debug("%8x", data);
+            switch ((data >> 8) & 0xFF)
+            {
+            case 0x78:
+                debug("+");
+                report.keys[0] = 0x80;
+                write_hid_report(&report);
+                sleep(10);
+                report.keys[0] = 0x00;
+                write_hid_report(&report);
+                break;
 
+            case 0x50:
+                debug("-");
+                report.keys[0] = 0x81;
+                write_hid_report(&report);
+                sleep(10);
+                report.keys[0] = 0x00;
+                write_hid_report(&report);
+                break;
+
+            default:
+                break;
+            }
+
+            event = 0;
+        }
         debug("disconnected\n");
     }
 }
